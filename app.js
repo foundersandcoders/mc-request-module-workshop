b2d7ea39efd7839ef6b9f9fae0e6bbc08bf9f3fb
Samantha Dickeson <samlucydickeson@gmail.com>
2019-07-15 23:05:17 +0100

    remove 'test' function in app.js

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
-"use strict";
-
-// This is the npm Request module that you are trying to emulated with `myRequest` below:
-const request = require('request');
-
 const myRequest = () => {
   /*
   create your own request module here.
-  It should take a url to make a http GET request, and a callback function with three arguments;
+  It should take a url to make a https GET request, and a callback function with two arguments;
   1. error (String: if an error occurred),
-  2. response(Object; includes the response & statusCode of the request),
-  3. body (String; includes the body of the request)
+  2. response(Object; includes the body & statusCode of the request)
   */
-}
-
-// Helper function - do not change this code! This is the test to check your myRequest function works the same as the Request npm module. 
-const testRequest = (module) => {
-  module('http://jsonplaceholder.typicode.com/users/1', function (error, response, body) {
-    console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('body:', body);
-  });
 };
 
-// Request module test
-testRequest(request);
-
-// myRequest module test
-testRequest(myRequest);
+module.exports = myRequest;
