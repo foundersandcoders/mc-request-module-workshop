73188318666ea069edce292453e5d6f906d263a1
Samantha Dickeson <samlucydickeson@gmail.com>
2019-07-23 14:22:41 +0100

    re-add 'use strict' and remove typo

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+"use strict";
+
 const myRequest = () => {
   /*
   create your own request module here.
-  It should take a url to make a https GET request, and a callback function with two arguments;
+  It should take a url to make a http GET request, and a callback function with two arguments;
   1. error (String: if an error occurred),
   2. response(Object; includes the body & statusCode of the request)
   */
