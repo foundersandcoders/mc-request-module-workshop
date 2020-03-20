5c4c90bbaee03dcc546054a98f6a4b18b9d155a9
oliverjam <oliverjamesphillips@gmail.com>
2020-03-20 11:52:24 +0000

    Use tape-promise to rewrite tests

--- app.test.js
+++ app.test.js
@@ -1,6 +1,8 @@
-const test = require("tape");
+const tape = require("tape");
 const nock = require("nock");
-const myRequest = require("./app");
+const _test = require("tape-promise").default;
+const test = _test(tape); // add promise support to tape
+const myRequest = require("./solution");
 
 test("myRequest fetches data correctly", t => {
   nock("http://jsonplaceholder.typicode.com")
@@ -8,10 +10,9 @@ test("myRequest fetches data correctly", t => {
     .reply(200, {
       name: "Leanne Graham",
     });
-  myRequest(
-    "http://jsonplaceholder.typicode.com/users/1",
-    (error, response) => {
-      t.error(error);
+
+  return myRequest("http://jsonplaceholder.typicode.com/users/1").then(
+    response => {
       t.equal(
         response.statusCode,
         200,
@@ -27,29 +28,32 @@ test("myRequest fetches data correctly", t => {
   );
 });
 
+test("myRequest rejects bad requests", t => {
+  return t.rejects(myRequest("abc"), "promise should reject on error");
+});
+
 // BONUS - uncomment this test to see if myRequest handles https successfully:
 
-// test("myRequest fetches data if API uses https", t => {
-// nock("https://jsonplaceholder.typicode.com")
-//   .get("/users/1")
-//   .reply(200, {
-//     name: "Leanne Graham"
-//   });
-//   myRequest(
-//     "https://jsonplaceholder.typicode.com/users/1",
-//     (error, response) => {
-//       t.error(error, "no https supported");
+// test("myBonusRequest fetches data if API uses https", t => {
+//   nock("http://jsonplaceholder.typicode.com")
+//     .get("/users/1")
+//     .reply(200, {
+//       name: "Leanne Graham",
+//     });
+
+//   return myRequest("http://jsonplaceholder.typicode.com/users/1").then(
+//     response => {
 //       t.equal(
 //         response.statusCode,
 //         200,
 //         "the API should respond with a status code of 200"
 //       );
 //       t.deepEqual(
-//   response.body.name,
-//   "Leanne Graham",
-//   "the response body should contain the correct json"
-// );
-// t.end();
+//         response.body.name,
+//         "Leanne Graham",
+//         "the response body should contain the correct json"
+//       );
+//       t.end();
 //     }
 //   );
 // });
